b592a5047f63956af9c6870e88c37138b8955b34
LIL_NAZ <carljhenriksson@gmail.com>
2024-02-24 14:21:08 +0100

    Added greeted starting

--- build/configureSystem.sh
+++ build/configureSystem.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# Privileges: Elevated need to be ran as root
 
 # switch shell
 echo "Chancing shell to ZSH"
 chsh -s /bin/zsh root
 chsh -s /bin/zsh $USER
+
+# enable tuigreeded
+systemctl enable greetd.service
